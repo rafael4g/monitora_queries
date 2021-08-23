25f31eab80a64c1ff3f7f309360797308a3e8624
Rafael D Silva <neromad@outlook.com>
2021-08-23 17:29:53 -0300

    new feature inn

--- get_file.bat
+++ get_file.bat
@@ -34,9 +34,12 @@ call set n_anomes=%%arquivo_original:~%new_length%,6%%
 call set n_arquivo=%%arquivo_original:~0,%new_length_arquivo%%%
 call set n_ext=%%arquivo_original:~%new_length_ext%,3%%
 
+echo teste_extencao
 IF "%n_ext%" EQU "txt" GOTO timed
 
 :timed
+echo esse %1
+
 IF "%1"=="" set set novo=0%1& Goto livrar
 
 IF %n_ext% EQU txt goto valida
@@ -50,9 +53,16 @@ IF EXIST %1.ini goto end
 IF NOT EXIST %1 goto end
 del /q %1
 copy NUL %arquivo_original%.ini
+::novo inn
+IF "%n_arquivo:~0,4%"=="INN_" goto proc_inn
+::atual dwh
 start /min call C:\Dados\colab\monitora\get_information.bat %n_arquivo% %n_anomes%
 EXIT
 )
+
+:proc_inn
+start /min call C:\Dados\colab\monitora\get_information_inn.bat %n_arquivo% %n_anomes%
+EXIT
 
 :livrar
 echo sair %novo%
--- get_information_inn.bat
+++ get_information_inn.bat
@@ -4,7 +4,6 @@ title EXECUTANDO %1 %2
 
 SET VAR_NOMEARQUIVO=%1
 SET VAR_ANOMES=%2
-::SET/a CHECK_ANOMES=%2
 
 set user="user_plsql"
 set pass="pass_plsql"
@@ -14,7 +13,21 @@ SET FILE=%LOCAL_PATH%\controle_data.txt
 
 SET LOCAL_QUERIES=C:\Dados\colab\queries\GENERAL
 SET QUERY_NAME=%LOCAL_QUERIES%\%VAR_NOMEARQUIVO%.sql
-
+:: Start Tratamento para nome do arquivo e anomes
+set #=%VAR_NOMEARQUIVO%
+set length=0
+set less_ext=2
+:loop
+if defined # (
+    rem Encurtar a corda por um caractere
+    set #=%#:~1%
+    rem incrementa a string contando a variavel %length%
+    set /A length += 1
+    rem repetir ate string ser null
+    goto loop
+)
+set /A new_length_ddd=%length% - %less_ext%
+call set nr_ddd=%%VAR_NOMEARQUIVO:~%new_length_ddd%,2%%
 
 FOR /F "tokens=1-7 delims=|" %%A in ('TYPE %FILE%') DO (
   IF %VAR_ANOMES% EQU %%A CALL :process %%A %%B %%C %%D %%E %%F %%G
@@ -23,15 +36,13 @@ FOR /F "tokens=1-7 delims=|" %%A in ('TYPE %FILE%') DO (
 :process
 echo %1
 IF %VAR_ANOMES% EQU %1 (
-SQLPLUS %user%/%pass%@INSTANCIA_PLSQL @%QUERY_NAME% %1 %2 %3 %4 %5 %6 %7
+SQLPLUS %user%/%pass%@INSTANCIA_PLSQL @%QUERY_NAME% %1 %2 %3 %4 %5 %6 %7 %nr_ddd%
 COPY C:\Dados\colab\extractions\%VAR_NOMEARQUIVO%_%VAR_ANOMES%.csv \\rede\%VAR_NOMEARQUIVO%_%VAR_ANOMES%.csv
-
 GOTO end
 EXIT
 )
 
 :end
-
 exit
 
 ::SET ANOMES=%1
